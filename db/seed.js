37fe61c7af610b12273a1609b018201927729b46
Aaliyah Augustus <aaliyahaugustus@yahoo.com>
2022-11-11 11:17:00 -0600

    part three

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,7 @@
-const { client, getAllUsers, createUser, updateUser, createPost, updatePost, getAllPosts, getPostsByUser } = require('./index');
+const { client, getAllUsers, createUser, updateUser, createPost, updatePost, getAllPosts, getPostsByUser, getUserById, createTags,
+  createPostTag,
+  addTagsToPost,
+  getPostById, getPostsByTagName} = require('./index');
 
 
 async function dropTables() {
@@ -6,6 +9,8 @@ async function dropTables() {
         console.log("Starting to drop tables...");
 
       await client.query(`
+      DROP TABLE IF EXISTS post_tags;
+      DROP TABLE IF EXISTS tags;
       DROP TABLE IF EXISTS posts;
       DROP TABLE IF EXISTS users;
       
@@ -39,8 +44,20 @@ async function dropTables() {
         content TEXT NOT NULL,
         active BOOLEAN DEFAULT true
       );
-      `);
+      
+      CREATE TABLE tags (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL
+      );
 
+      CREATE TABLE post_tags (
+        "postId" INTEGER REFERENCES posts(id),
+        "tagId" INTEGER REFERENCES tags(id),
+        UNIQUE ("postId", "tagId")
+      );
+  
+      `);
+        // UNIQUE is preventing creating dulipcates of postid & tagid combination
       console.log("Finished building tables!");
     } catch (error) {
         console.error("Error building tables!")
@@ -74,19 +91,24 @@ async function dropTables() {
     await createPost({
       authorId: albert.id,
       title: "First Post",
-      content: "This is my first post. I hope I love writing blogs as much as I love writing them."
+      content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
+      tags: ["#happy", "#youcandoanything"]
     });
 
     await createPost({
       authorId: sandra.id,
       title: "How does this work?",
-      content: "Seriously, does this even do anything?"
+      content: "Seriously, does this even do anything?",
+      tags: ["#happy", "#worst-day-ever"]
+      
     });
 
     await createPost({
       authorId: glamgal.id,
       title: "Living the Glam Life",
-      content: "Do you even? I swear that half of you are posing."
+      content: "Do you even? I swear that half of you are posing.",
+      tags: ["#happy", "#youcandoanything", "#canmandoeverything"]
+      
     });
   
       console.log("Finished creating posts.")
@@ -95,6 +117,7 @@ async function dropTables() {
       throw error;
     }
   }
+
   
   async function rebuildDB() {
     try {
@@ -104,11 +127,13 @@ async function dropTables() {
       await createTables();
       await createInitialUsers();
       await createInitialPosts();
+     
     } catch (error) {
       console.log("Error during the rebuildDB")
     throw error;
      } 
     }
+
   
   async function testDB() {
     try {
@@ -129,16 +154,19 @@ async function dropTables() {
         const posts = await getAllPosts();
         console.log("Result:", posts);
 
-        console.log("Calling updatePost on posts[0]");
-        const updatePostResult = await updatePost(posts[0].id, {
-          title: "New Title",
-          content: "Updated Content"
+        console.log("Calling updatePost on posts[1], only updating tags");
+        const updatePostTagsResult = await updatePost(posts[1].id, {
+          tags: ["#youcandoanything", "#redfish", "#bluefish"]
         });
-        console.log("Result:", updatePostResult);
+        console.log("Result:", updatePostTagsResult);
 
         console.log("Calling getUserById with 1");
         const albert = await getUserById(1);
         console.log("Result:", albert);
+
+        console.log("Calling getPostsByTagName with #happy");
+        const postsWithHappy = await getPostsByTagName("#happy");
+        console.log("Result:", postsWithHappy);
 
 
     console.log("Finished database tests!");
