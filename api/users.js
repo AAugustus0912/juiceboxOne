418d8c470175c9e7cbcd7e9fe615072208c9a166
Aaliyah Augustus <aaliyahaugustus@yahoo.com>
2022-11-19 16:51:38 -0600

    part 2 complete

--- api/users.js
+++ api/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const usersRouter = express.Router();
 const jwt = require('jsonwebtoken');
-
+const { getAllUsers, getUserByUsername, createUser } = require('../db');
 
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -9,7 +9,6 @@ usersRouter.use((req, res, next) => {
   next(); 
 });
 
-const { getAllUsers } = require('../db');
 usersRouter.get('/', async (req, res) => {
     const users = await getAllUsers();
 
@@ -18,9 +17,8 @@ usersRouter.get('/', async (req, res) => {
   });
 });
 
-const { getUserByUsername } = require('../db');
 usersRouter.post('/login', async (req, res, next) => {
-    const { username, password } = (req.body);
+    const { username, password} = (req.body);
 
     if (!username || !password) {
         next({
@@ -53,6 +51,42 @@ usersRouter.post('/login', async (req, res, next) => {
         console.log(error);
         next(error);
       }
+    });
+
+    usersRouter.post('/register', async (req, res, next) => {
+      const { username, password, name, location } = req.body;
+    
+      try {
+        const _user = await getUserByUsername(username);
+    
+        if (_user) {
+          next({
+            name: 'UserExistsError',
+            message: 'A user by that username already exists'
+          });
+        }
+    
+        const user = await createUser({
+          username,
+          password,
+          name,
+          location,
+        });
+    
+        const token = jwt.sign({ 
+          id: user.id, 
+          username
+        }, process.env.JWT_SECRET, {
+          expiresIn: '1w'
+        });
+    
+        res.send({ 
+          message: "thank you for signing up",
+          token 
+        });
+      } catch ({ name, message }) {
+        next({ name, message })
+      } 
     });
 
 
